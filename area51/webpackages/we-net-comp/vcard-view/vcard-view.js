a28f20a8566a047c548885060200de568c821613
Michael Szwarc <michael.szwarc@incowia.com>
2017-06-26 18:29:14 +0200

    cleanup comp

--- area51/webpackages/we-net-comp/vcard-view/vcard-view.js
+++ area51/webpackages/we-net-comp/vcard-view/vcard-view.js
@@ -1,58 +1,25 @@
 (function () {
-  'use strict';
-  /**
-   * Get help:
-   * > Lifecycle callbacks:
-   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
-   *
-   * Access the Cubbles-Component-Model:
-   * > Access slot values:
-   * slot 'a': this.getA(); | this.setA(value)
-   */
-  CubxPolymer({
-    is: 'vcard-view',
 
-    /**
-     * Manipulate an element’s local DOM when the element is created.
-     */
-    created: function () {
-    },
+	'use strict';
 
-    /**
-     * Manipulate an element’s local DOM when the element is created and initialized.
-     */
-    ready: function () {
-      // set value-attribute of element with id='slota' to the initial value of slot 'a'
-      this.$.slota.setAttribute('value', this.getA());
-    },
+	CubxPolymer({
+		is: 'vcard-view',
 
-    /**
-     * Manipulate an element’s local DOM when the element is attached to the document.
-     */
-    attached: function () {
-    },
+		created: function () {},
 
-    /**
-     * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
-     */
-    cubxReady: function () {
-    },
+		ready: function () {},
 
-    /**
-     * A handler to be called by a dom-element
-     * @param {event} event
-     */
-    inputFieldSlotAChanged: function (event) {
-      // update the cubbles-model
-      this.setA(event.target.value);
-    },
+		attached: function () {},
 
-    /**
-     *  Observe the Cubbles-Component-Model: If value for slot 'a' has changed ...
-     */
-    modelAChanged: function (newValue) {
-      // update the view
-      this.$.slota.setAttribute('value', newValue);
-    }
-  });
-}());
+		cubxReady: function () {},
+
+		modelConfigChanged: function (newValue) {
+			// TODO
+		},
+		
+		modelUseridChanged: function (newValue) {
+			// TODO
+		}
+	});
+}
+	());
